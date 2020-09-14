13f5c8ece534eb875cb40cfb97d68f10a85fc259
solbencas <cecibenitezca@gmail.com>
2020-09-14 16:12:27 -0400

    Add main.rb inside the bin folder and create content and instantiate the bot.

---
+++ bin/main.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/bot'
+require_relative '../lib/motivate'
+
+puts 'Hello!, Welcome to Ceci telegram bot'
+puts '----------------------------------------------'
+puts 'This is a bot to help you stay motivated by giving you motivational quotes'
+puts 'The bot can also keep you entertained by providing you with jokes'
+puts 'This bot will help you when you feeling down and demotivated'
+sleep(0.6)
+puts '----------------------------------------------'
+puts '...loading the bot is now online... use ctrl-c to stop the bot'
+Bot.new
