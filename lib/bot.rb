db62dae3b75c9f759baa15ce908d167da7a7213d
solbencas <cecibenitezca@gmail.com>
2020-09-14 13:25:05 -0400

    Create the Bot class.

---
+++ lib/bot.rb
@@ -0,0 +1,9 @@
+require 'telegram/bot'
+require_relative 'motivate.rb'
+require_relative 'joke.rb'
+
+class Bot
+  def initialize
+    token = '1318191703:AAHqVe_Vqw4decD1v0Kzj0PE3IEcpK8VY4g'
+  end
+end
