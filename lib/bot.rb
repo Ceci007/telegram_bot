139c1ba92c394330b970a0b5002a3e3e331cd051
solbencas <cecibenitezca@gmail.com>
2020-09-14 13:30:07 -0400

    Add the case for the /start with the Telegram Bot API in the Bot class.

--- lib/bot.rb
+++ lib/bot.rb
@@ -6,4 +6,13 @@ class Bot
   def initialize
     token = '1318191703:AAHqVe_Vqw4decD1v0Kzj0PE3IEcpK8VY4g'
   end
+
+  Telegram::Bot::Client.run(token) do |bot|
+    bot.listen do |message|
+      case message.text
+      when '/start'
+
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} , welcome to Ceci chat bot created by peter robert, the chat bot is to keep you entertained. Use  /start to start the bot,  /stop to end the bot, /motivate to get a diffrent motivational quote everytime you request for it or /joke to get a joke everytime you request for it")
+    end
+  end
 end
