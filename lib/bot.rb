c86bc4d7dbdf1c2ee45850fa08a9003646a48e53
solbencas <cecibenitezca@gmail.com>
2020-09-14 13:38:29 -0400

    Add the /motivate case to the Bot class.

--- lib/bot.rb
+++ lib/bot.rb
@@ -11,11 +11,15 @@ class Bot
     bot.listen do |message|
       case message.text
       when '/start'
-
         bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} , welcome to Ceci chat bot created by peter robert, the chat bot is to keep you entertained. Use  /start to start the bot,  /stop to end the bot, /motivate to get a diffrent motivational quote everytime you request for it or /joke to get a joke everytime you request for it")
+      
       when '/stop'
-
         bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)
+      
+      when '/motivate'
+        values = Motivate.new
+        value = values.random
+        bot.api.send_message(chat_id: message.chat.id, text: "#{value['text']}", date: message.date)
       end
   end
 end
