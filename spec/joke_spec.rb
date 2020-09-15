580b7994e3075826f8c1a4ca718c41173d625bef
solbencas <cecibenitezca@gmail.com>
2020-09-15 12:53:20 -0400

    Add test case for Hash:
    The test should return a Hash if the request is successful.

--- spec/joke_spec.rb
+++ spec/joke_spec.rb
@@ -4,5 +4,9 @@ describe Joke do
   describe '#request' do
     let(:request) { Joke.new }
     let(:request_json) { request.request }
+
+    it 'should return hash response when the request is sucessful' do
+      expect(request_json.class).to eql(Hash)
+    end
   end
 end
